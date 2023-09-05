9a9192afdcdc3ef514365cefbf39144d674d3e07
Abner <58574013+Abnersampf@users.noreply.github.com>
2023-09-05 18:04:33 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,6 @@
 const num = prompt('Digite um valor:');
 const base = prompt('Digite a base para qual deseja converter [2 - Binário ou 10- Decimal]:');
-if (base == '2'){
+if (base == '2')
     alert(`${num} (dec) = ${(parseInt(num)).toString(2)} (bin)`);
-}
-else{
+else
     alert(`${num} (bin) = ${parseInt(num, 2)} (dec)`);
-}
